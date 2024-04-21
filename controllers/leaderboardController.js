731a8ac1a962b32142e16aa5026bc663fe910095
ChrisCoetzer <christophercoetzer322@gmail.com>
2024-04-21 16:22:23 -0400

    Changed leadboard to show first and last name instead of email(internal username)

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -1,21 +1,34 @@
 const UserScore = require('../models/userScore').model;
+const User = require('../models/user');
 
 // Aggregates scores and returns the leaderboard
 const getLeaderboard = async () => {
     const leaderboard = await UserScore.aggregate([
         {
             $group: {
-                _id: "$username", // Group by username
-                totalScore: { $sum: "$score" }, // Sum scores for each username
+                _id: "$userId", // Group by userId
+                totalScore: { $sum: "$score" }, // Sum scores for each userId
             },
+        },
+        {
+            $lookup: { // Join with User collection
+                from: "users", 
+                localField: "_id", 
+                foreignField: "_id", 
+                as: "userDetails", // The array where the matched User documents will be added
+            },
+        },
+        {
+            $unwind: "$userDetails", // Flatten the userDetails array
         },
         {
             $sort: { totalScore: -1 }, // Sort by totalScore in descending order
         },
         {
-            $project: { // Adjust the output format, if needed
-                _id: 0, 
-                username: "$_id", 
+            $project: { // Define the output format
+                _id: 0,
+                firstName: "$userDetails.firstName",
+                lastName: "$userDetails.lastName",
                 totalScore: 1,
             },
         },
@@ -23,6 +36,7 @@ const getLeaderboard = async () => {
 
     return leaderboard;
 };
+
 
 module.exports = {
     getLeaderboard,
